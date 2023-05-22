39bcc6ea1ccca63baa553c75c09bc448491e4406
Deniz <denizpekova21@outlook.com>
2023-05-22 10:32:03 +0300

    Yeni Proje Started

---
+++ DataAccessLayer/Context/KContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+
+namespace DataAccessLayer.Context
+{
+    public class KContext : IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-I45D279;database=AcıkolejiKutuphane; Integrated security=true");
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<IdentityUserLogin<string>>().HasNoKey();
+            builder.Entity<IdentityUserRole<string>>().HasNoKey();
+            builder.Entity<IdentityUserToken<string>>().HasNoKey();
+        }
+
+        public DbSet<Kitap> kitaps { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Kitap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Kitap
+    {
+        public int KitapID { get; set; }
+        public string KitapName { get; set; }
+        public string  KitapYazarName { get; set; }
+        public string KitapSayfa { get; set; }
+        public string KitapYayınEvi { get; set; }
+        public DateTime KitapBasımTarih { get; set; }
+    }
+}
